f5131a215fcc6ec7d74b0deb0e7740f549b3c64e
GUNEET SINGH <guneetsingh@GUNEETs-MacBook-Air.local>
2021-11-12 17:07:50 +0530

    User Auth Middleware

---
+++ Middleware/userAuth.js
@@ -0,0 +1,19 @@
+var jwt = require('jsonwebtoken');
+
+const authorise = async (req, res, next) => {
+    const success = false
+    const id = req.header('authtoken')
+    if (!id) {
+        return res.status(401).send({ success,message: "You are not authorised" })
+    }
+    try {
+        const decode = jwt.verify(id, privateKey)
+        req.user= decode.id
+        next()
+    } catch (err) {
+        res.status(401).send({ success,message: "Problem with token" })
+    }
+
+}
+
+module.exports = authorise
